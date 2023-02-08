01a6411532c755030b72df0b281a1000c1e3fd8c
Isaac M Bazian <bazia001@umn.edu>
2023-02-08 16:08:21 -0600

    Created import_tbl and wide_tbl

--- R/week4.R
+++ R/week4.R
@@ -2,3 +2,8 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
+#Data Import
+import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+glimpse(import_tbl)
+wide_tbl <- separate(import_tbl, col = qs, into = c("q1", "q2", "q3", "q4", "q5"), sep = " - ")
+
